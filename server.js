77ba2c90c8a33f390ea91c6448dab2b4f743b827
zepinghe <zepinghe@umd.edu>
2020-10-01 22:32:08 -0400

    auto

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+const AWS = require("aws-sdk");
+
+// connect to dynamoDB
+AWS.config.update({ region: "us-east-1" });
+var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 const app = express();
 
@@ -13,15 +18,41 @@ app.get("/", (_, res) => {
 app.get("/locations", async (req, res) => {
   //   var ip = req.headers["x-forwarded-for"] || req.connection.localAddress;
   const ip_1 = req.ip.replace("::ffff:", "");
+  var url = "";
+
   try {
     const token = "bf659e5b6f895e";
-    const url = "http://ipinfo.io/" + ip_1 + "?token=" + token;
+    url = "http://ipinfo.io/" + ip_1 + "?token=" + token;
 
     const ipres = await axios.get(url);
     console.log(ipres);
     res.json(ipres.data);
+    loc = ipres.data.loc;
+    lat = loc.split(",")[0];
+    long = loc.split(",")[1];
+    // Call DynamoDB to add the item to the table
+    var params = {
+      TableName: "esri_demp",
+      Item: {
+        ip_addr: { N: ip_1 },
+        ISP: { S: ipres.data.org },
+        VisitorCount: { N: "1" },
+        Lat: { N: lat },
+        Long: { N: long },
+        LastVisitTime: { N: "1" },
+      },
+    };
+
+    ddb.putItem(params, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
   } catch (error) {
     console.log(error);
+    res.status(500).json("Internal Service Error");
   }
 });
 
