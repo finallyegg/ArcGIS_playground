7091b0043d9cadeeb5a24e4bfc267fc8072adbf2
zepinghe <zepinghe@umd.edu>
2020-10-01 22:59:56 -0400

    auto

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get("/locations", async (req, res) => {
       },
     };
     var result = await ddb.getItem(getParam).promise();
-    console.log(JSON.stringify(result));
-
+    // console.log(JSON.stringify(result));
+    console.log(result.Item.length());
     // data = await ddb.getItem(getParam, function (err, data) {
     //   if (err) {
     //     console.log("Error", err);
